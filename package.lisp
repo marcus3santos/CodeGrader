5be160ade2193c431172a820d66bafd8bfc1e6f0
Marcus Santos <34948880+marcus3santos@users.noreply.github.com>
2024-12-05 20:32:45 -0500

    Add files via upload

--- package.lisp
+++ package.lisp
@@ -17,12 +17,6 @@
   (:export :evaluate-solution)
   (:export :grade-code))
 
-#|
-(defpackage #:codemeter
-  (:documentation "Measures program size")
-  (:use cl :test-runtime)
-  (:export :program-size))
-|#
 
 (defpackage #:codegrader
   (:documentation "Manages the grading of all assignment submissions")
@@ -34,6 +28,18 @@
   (:use :common-lisp :cl-user :codegrader :grader)
   (:export :start :quit :eval-solutions :grade-it  :grade-exam :evaluate-solution :eval-student-solutions :ck-my-solution))
 
+#|
+(defpackage :q_i
+  (:documentation "Dedicated package for the student's solution store in file q_i,lisp, so it does not polute CodeGrader's name space")
+  (:use cl)
+  (:export <All functions asked in a question>))
 
+The idea here is to have a qpackages.lisp file containing one defpackage for each question of an assessment.
 
+The folder containing the defpackages for an assessment will be provided as a new argument to cg:grade-exam ;
+
+Codegrader will load all defpackages before loading and running the student's solutions.
+
+Then, in the test cases file for a given question, q_i, where a given function func is tested, we will refer to that function by mentioning its package q_i:func
+|#
 
