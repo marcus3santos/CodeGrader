b43a44571b12053420c5cd452009932825f3488f
Marcus Santos <34948880+marcus3santos@users.noreply.github.com>
2025-01-14 12:20:59 -0500

    Delete Example/Gen-files/Packages/q1.lisp

--- Example/Gen-files/Packages/q1.lisp
+++
@@ -1,5 +0,0 @@
-
-(DEFPACKAGE :Q1
-  (:DOCUMENTATION "Dedicated package for the student's solution store, so it does not polute CodeGrader's name space")
-  (:USE CL)
-  (:EXPORT COUNT-OCCURRENCES CONTAINS-ALL))
