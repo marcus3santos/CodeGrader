3469a21c142f467286f3bc5545e62ffb8a9247c6
Marcus Santos <34948880+marcus3santos@users.noreply.github.com>
2025-04-08 17:19:57 -0400

    Add files via upload

--- sexprmarkup.lisp
+++ sexprmarkup.lisp
@@ -23,36 +23,75 @@
                          (list (string-right-trim " " (car (last strings)))))))
     trimmed))
 
+(defun getkey-value (plist indic)
+  (multiple-value-bind (i v tail)
+      (get-properties plist indic)
+    (values i v tail)))
 ;; Serializer
 
 (defun sexprmark->org (sexpr)
   (labels
-      ((emit (node &key qnumber (depth 0))
-         "qnumber is the question number, and depth is space indentation"
+      ((emit (node &key qnumber penalty forbidden (depth 0))
+         "qnumber is the question number, penalty is the percentage deduction on a solution,
+          forbidden is a list of forbidden functions, and depth is space indentation"
          (cond
            ((consp node)
             (case (car node)
               (doc
-               (let ((title (getf (cdr node) :title))
-                     (children (cdddr node)))
-                 (cons (format nil "#+TITLE: ~a~%" title)
+               (let* ((proplist (cadr node))
+                      (title (getf proplist :title))
+                      (children (if title (nthcdr 2 node)
+                                    (error "Missing document title in ~s" node))))
+                 (cons (format nil "#+TITLE: ~s~%" title)
                        (mapcar #'emit children))))             
               (section
-               (let ((level (getf (cdr node) :level))
-                     (title (getf (cdr node) :title))
-                     (children (cdr (cddddr node))))
-                 (cons (format nil "~a ~a~%" (stars level) title)
-                       (mapcar #'emit children))))
+               (let* ((proplist (cadr node))
+                      (level (getf proplist :level))
+                      (title (if level (getf proplist :title)
+                                 (error "Missing section level in ~s" node)))
+                      (children (if title (nthcdr 2 node)
+                                    (error "Missing section title in ~s" node))))
+                 (cons (emit '(p) :qnumber qnumber :penalty penalty :forbidden forbidden :depth depth)
+                       (cons (format nil "~a ~a~%" (stars level) title)
+                             (mapcar (lambda (item)
+                                       (emit item :qnumber qnumber :penalty penalty :forbidden forbidden :depth depth))
+                                     children)))))
               (question
-               (let ((title (getf (cdr node) :title))
-                     (number (getf (cdr node) :number))
-                     (children (cdr (cddddr node))))
-                 (cons (format nil "~a ~a ~a~%" (stars 1) title number)
-                       (mapcar (lambda (item)
-                                 (emit item :qnumber number))
-                               children))))              
+               (let* ((proplist (cadr node))
+                      (title (getf proplist :title))
+                      (number (if title (getf proplist :number)
+                                  (error "Missing question title in ~s" node)))
+                      (penalty (if number (getf proplist :penalty)
+                                   (error "Missing question number in ~s" node)))
+                      (forbidden (getf proplist :forbidden))
+                      (children (cond ((and forbidden (or (not penalty) (= penalty 0)))
+                                       (error "You forgot to provide the penalty in ~s" node))
+                                      ((and penalty (not (= penalty 0)) (not forbidden))
+                                       (error "You forgot to provide the list of forbidden functions in ~s" node))
+                                      (t (nthcdr 2 node)))))
+                 (cons (emit '(p) :qnumber qnumber :penalty penalty :forbidden forbidden :depth depth)
+                       (cons (format nil "~a ~a ~a~%" (stars 1) title number)
+                             (mapcar (lambda (item)
+                                       (emit item :qnumber number :penalty penalty :forbidden forbidden :depth depth))
+                                     children)))))
+              (whats-asked
+               (append
+                (list (emit '(p (b "WHAT YOU ARE ASKED:")))
+                      (emit '(p))
+                      (emit '(p (b "NOTE:")))
+                      (emit `(ul
+                              (li "You" are required to write the solutions for the parts of this question in the Lisp program file "*~a/q~a.lisp*")
+                              (li "You" may create helper functions in your program file.)
+                              ,(if forbidden
+                                   `(li "You" must not use or refer to the following Lisp built-in "function(s)" and "symbol(s):" ,(format nil "~a" forbidden) ".  The" penalty for doing so is a deduction of (b ,penalty percent) on the score of your solutions for this question.)
+                                   `(li "There" are no restrictions in the use of Lisp built-in functions or symbols in the parts of this question.)
+                                   ))))
+                (mapcar (lambda (item)
+                          (emit item :qnumber qnumber :penalty penalty :forbidden forbidden :depth depth))
+                        (cdr node))))
               (p
-               (append (mapcar #'emit (cdr node))
+               (append (cons (format nil "~%")
+                             (mapcar #'emit (cdr node)))
                        (list (format nil "~%"))))
               (ul
                (mapcar (lambda (item)
@@ -104,19 +143,20 @@
 
 (defun test ()
   (let ((sexpr
-          '(doc :title "PT 1"
-            (section :level 1 :title "Heading 1"
+          '(doc (:title "PT 1")
+            (section  (:title "Heading 1" :level 1)
              (p "Some paragraph text under \"a\" heading 1.")
              (p "This" is a text.))
-            (question :title "Question" :number 1
-             (example-block
-              (example (fact 3) 6)
-              (example (fact 0) 1))
-             (p "Question" comes here.))
-            (question :title "Question" :number 2
+            (question (:title "Question" :number 1 :penalty 90 :forbidden ( a b c))
+             (whats-asked
+              (p "Question" comes here.)
+              (example-block
+               (example (fact 3) 6)
+               (example (fact 0) 1))))
+            (question (:title "Question" :penalty 10 :number 2 :forbidden (a))
              (p "Question" comes here.)
              )
-            (section :level 2 :title "Heading 2"
+            (section (:level 2 :title "Heading 2")
              (p "Another" paragraph  "$x=2$"  and (em the rest.))
              (P "Write" a (tt function)  (b count-occurrences and another) )
              (ul
